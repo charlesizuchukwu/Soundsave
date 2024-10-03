eabf855203ec248a563e25d359e872cafc9d207c
godsonizhere <kizicharles001@gmail.com>
2024-10-02 21:42:00 -0700

    changed

--- frontend/soundsave-frontend/src/api/axios.js
+++ frontend/soundsave-frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "http://127.0.0.1:5000";
 
-const BASE_URL = "https://soundsave-server.vercel.app";
+const BASE_URL = "https://soundsave-server.vercel.app/";
 
 export default axios.create({
   baseURL: BASE_URL,
